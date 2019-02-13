da45b76dd1d760564223813940fcc5164a8dbdd7
navarenjini20 <45868307+navarenjini20@users.noreply.github.com>
2019-02-13 15:11:23 +0530

    Create find no first 5 no.py

---
+++ find no first 5 no.py
@@ -0,0 +1,9 @@
+def multiple(m, n): 
+   # inserts all elements from n to  
+   # (m * n)+1 incremented by n. 
+   a = range(n, (m * n)+1, n) 
+   print(*a)
+# driver code  
+m = 4
+n = 3
+multiple(m, n) 
